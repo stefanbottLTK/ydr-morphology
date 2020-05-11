eca0d2d3725cbaf9c522d9a1ebf361c5ad0e125c
stefanbottLTK <stefan.bott@lovetoknow.com>
2020-05-11 16:30:17 +0200

    minor changes

--- GetMorphFromDB.py
+++ GetMorphFromDB.py
@@ -3,53 +3,65 @@ import json
 
 def get_form_from_db(event, context):
     event_body = json.loads(event['body'])
-    lemma = (event_body['lemma'])
-    pos = (event_body['pos'])
-    passw = (event_body['pass'])
-    results_list = []
-    try:
-        connection = psycopg2.connect(user = "yourdictionary",
-                                      password = passw,
-                                      host = "yourdictionary-dev.cqtzt424uvng.us-east-1.rds.amazonaws.com",
-                                      port = "5432",
-                                      database = "semantic_vectors")
-
-        cursor = connection.cursor()
-        # Print PostgreSQL Connection properties
-        # print ("hello world")
-        # print ( connection.get_dsn_parameters(),"\n")
-        # Print PostgreSQL version
-        # cursor.execute("SELECT version();")
-        # record = cursor.fetchone()
-        # print("You are connected to - ", record,"\n")
-        print("")
-        sql_sting = (f"SELECT wordform FROM morphology WHERE lemma='{lemma}' and postag='{pos}' ")
-        print(sql_sting)
-        cursor.execute(sql_sting)
-        # print(cursor.fetchone()[0])
-        rows = cursor.fetchall()
-        print(rows)
-        for row in rows:
-            # print(row[0] + "\t..")
-            this_result = {"form" : row[0]}
-            results_list.append(this_result)
-        print("")
-        connection.commit()
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                # print("PostgreSQL connection is closed")
 
     response = {
-        'statusCode': 200,
-        'body': json.dumps(results_list)
+        "statusCode": 200,
+        "body" : "{\"pos\":\"VVZ\",\"lemma\": \"dog\"}"
     }
     return response
+    # lemma = (event_body['lemma'])
+    # pos = (event_body['pos'])
+    # passw = (event_body['pass'])
+    # results_list = []
+    # query_params = (
+    #     event["queryStringParameters"]
+    #     if "queryStringParameters" in event and event["queryStringParameters"]
+    #     else {}
+    # )
+    #
+    # try:
+    #     connection = psycopg2.connect(user = "yourdictionary",
+    #                                   password = passw,
+    #                                   host = "yourdictionary-dev.cqtzt424uvng.us-east-1.rds.amazonaws.com",
+    #                                   port = "5432",
+    #                                   database = "semantic_vectors")
+    #
+    #     cursor = connection.cursor()
+    #     # Print PostgreSQL Connection properties
+    #     # print ("hello world")
+    #     # print ( connection.get_dsn_parameters(),"\n")
+    #     # Print PostgreSQL version
+    #     # cursor.execute("SELECT version();")
+    #     # record = cursor.fetchone()
+    #     # print("You are connected to - ", record,"\n")
+    #     print("")
+    #     sql_sting = (f"SELECT wordform FROM morphology WHERE lemma='{lemma}' and postag='{pos}' ")
+    #     print(sql_sting)
+    #     cursor.execute(sql_sting)
+    #     # print(cursor.fetchone()[0])
+    #     rows = cursor.fetchall()
+    #     print(rows)
+    #     for row in rows:
+    #         # print(row[0] + "\t..")
+    #         this_result = {"form" : row[0]}
+    #         results_list.append(this_result)
+    #     print("")
+    #     connection.commit()
+    #
+    # except (Exception, psycopg2.Error) as error :
+    #     print ("Error while connecting to PostgreSQL", error)
+    # finally:
+    #     #closing database connection.
+    #         if(connection):
+    #             cursor.close()
+    #             connection.close()
+    #             # print("PostgreSQL connection is closed")
+    #
+    # response = {
+    #     'statusCode': 200,
+    #     'body': json.dumps(results_list)
+    # }
+    # return response
 
 
 def get_analyisis_from_db(event, context):
